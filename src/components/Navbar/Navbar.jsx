9acb0866980a480e0e0c7ad3d2ca85709bb1f050
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-19 19:41:23 +0200

    Navbar + packages

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+        <h2 className="Brand">SurvivorShop</h2>
+        <ul>
+            <li>PorfileIcon</li>
+            <li>CartIcon</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
