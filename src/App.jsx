8d69321961cc52ff4c78ea861824fb3b94906500
alextebbitt <alex.tebbitt87@gmail.com>
2022-06-20 14:35:18 +0200

    started not working 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import './App.css';
 import { BrowserRouter , Routes , Route} from 'react-router-dom';
 import { ProductProvider } from "./context/ProductsContext/ProductState";
+import { UserProvider } from "./context/UserContext/UserState";
 import LandingPage from './components/LandingPage/LandingPage';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer'
 import UserPage from './components/UserPage/UserPage';
 import Store from './components/Store/Store'
+import Login from "./components/Login/Login";
 
 function App() {
-    return ( 
-        <div className = "App" >
-            <BrowserRouter>
-                <ProductProvider>
-                    <Navbar/>
-                    <Routes>
-                        <Route path='/' element={<LandingPage/>}/>
-                        <Route path='/user/:id' element={<UserPage/>}/>
-                        <Route path='/store' element={<Store/>}/>
-                    </Routes>
-                    <Footer/>    
-                </ProductProvider>
-            </BrowserRouter>
-        </div>
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <UserProvider>
+            <ProductProvider>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/user/:id" element={<UserPage />} />
+                <Route path="/store" element={<Store />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+              <Footer />
+            </ProductProvider>
+          </UserProvider>
+        </BrowserRouter>
+      </div>
     );
 }
 
