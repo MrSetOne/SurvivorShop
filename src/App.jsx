2da63c28b9e786984dde0b30e31e22d60c8e4324
alextebbitt <alex.tebbitt87@gmail.com>
2022-06-21 14:18:01 +0200

    orders hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-import './App.css';
+import "./App.css";
 import "antd/dist/antd.css";
 
-import { BrowserRouter , Routes , Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductProvider } from "./context/ProductsContext/ProductState";
 import { UserProvider } from "./context/UserContext/UserState";
-import LandingPage from './components/LandingPage/LandingPage';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer'
-import UserPage from './components/UserPage/UserPage';
-import Store from './components/Store/Store'
+import LandingPage from "./components/LandingPage/LandingPage";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import UserPage from "./components/UserPage/UserPage";
+import Store from "./components/Store/Store";
 import Login from "./components/Login/Login";
-import SignUp from './components/SignUp/SignUp';
-import UpdateUser from './components/UpdateUser/UpdateUser';
+import SignUp from "./components/SignUp/SignUp";
+import UpdateUser from "./components/UpdateUser/UpdateUser";
 import Cart from "./components/Cart/Cart";
+import { OrdersProvider } from "./context/OrdersContext/OrderState";
 
 function App() {
-    return (
-      <div className="App">
-        <BrowserRouter>
+  return (
+    <div className="App">
+      <BrowserRouter>
           <UserProvider>
             <ProductProvider>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/user/:id" element={<UserPage />} />
-                <Route path="/store" element={<Store />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/update" element={<UpdateUser />} />
-                <Route path="/cart" element={<Cart />} />
-              </Routes>
-              <Footer />
+              <OrdersProvider>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/user/:id" element={<UserPage />} />
+                  <Route path="/store" element={<Store />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/update" element={<UpdateUser />} />
+                  <Route path="/cart" element={<Cart />} />
+                </Routes>
+                <Footer />
+              </OrdersProvider>
             </ProductProvider>
           </UserProvider>
         </BrowserRouter>
-      </div>
-    );
+    </div>
+  );
 }
 
 export default App;
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import { ProductContext } from "../../context/ProductsContext/ProductState";
+import { OrdersContext } from "../../context/OrdersContext/OrderState";
 
 const Cart = () => {
   const { cart, clearCart } = useContext(ProductContext);
+  const { createOrder } = useContext(OrdersContext);
 
   if (cart.length <= 0) {
     return <span>No tienes ningún producto añadido</span>;
   }
+
+const createNewOrder = () => {
+  createOrder(cart);
+  clearCart();
+};
 
   const cartItem = cart.map((cartItem, i) => {
     return (
@@ -18,10 +25,12 @@ const Cart = () => {
   });
 
   return (
-  <div>
-    {cartItem}
-    <button onClick={() => clearCart()}>Clear cart</button>
-  </div>);
+    <div>
+      {cartItem}
+      <button onClick={() => clearCart()}>Clear cart</button>
+      <button onClick={() => createNewOrder()}>Create Order</button>
+    </div>
+  );
 };
 
 export default Cart;
