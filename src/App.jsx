e6dcd277714ba92e686b3e3b85bd2db33c5282f1
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-22 12:53:09 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import "antd/dist/antd.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserContext/UserState";
-import {UserOutlined, SmileOutlined, ShoppingCartOutlined} from "@ant-design/icons"
+import {UserOutlined, LogoutOutlined, ShoppingCartOutlined} from "@ant-design/icons"
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
@@ -15,20 +15,29 @@ const navigate = useNavigate()
   }
 
   return (
-    <nav className="Navbar">
-      <h2 className="Brand">SurvivorShop</h2>
-      <div className="links">
-        <Link to="/">Home</Link>
-        {token?
-        <div style={{display:"flex"}}>
-          <Link to="/user">{username}</Link>
-          <p onClick={()=>doLogout()}>logout</p>
-          <Link to="/cart"><ShoppingCartOutlined/></Link>
+    <div className="Navbar__container">
+      <nav className="Navbar">
+        <h2 className="Brand">SurvivorShop</h2>
+        <div className="links">
+          {token?
+          <div className="navigate">
+            <Link to="/cart"><ShoppingCartOutlined/></Link>
+            <Link to="/user">
+              <div className="user__icon">
+                <p>{username.charAt(0)}</p>
+              </div>
+            </Link>
+            <p onClick={()=>doLogout()}>{<LogoutOutlined />}</p>
+          </div>
+          :
+          <Link to="/logpage"><UserOutlined/></Link>}
         </div>
-        :
-        <Link to="/logpage"><UserOutlined/></Link>}
-      </div>
-    </nav>
+      </nav>
+      <nav className="SubNavBar">
+      <Link to="/">Home</Link>
+      <Link to="/store">Store</Link>          
+      </nav>
+    </div>
   );
 };
 
