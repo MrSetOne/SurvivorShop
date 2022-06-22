ec5f3e0ba436e23c75da787ce90917ae4ad4b358
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-22 17:18:43 +0200

    landingpage done

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,21 @@
+import './LandingPage.scss'
+
 const LandingPage = () => {
   return (
     <main className="LandingPage">
-        <h1>Contenido de la LandingPage</h1>
+        <h1 className='LandingPage__title'>Still alive</h1>
+        <section className='LandingPage__container'>
+          <article className='LandingPage__knifes'>
+            <h2>3x2 in out knifes</h2>
+          </article>
+          <article className='LandingPage__car'>
+            <h2>Comming soon</h2>
+          </article>
+          <article className='LandingPage__puppy'>
+            <h2>New Wardog!</h2>
+            <p>It's the utimate killmachine!</p>
+          </article>
+        </section>
     </main>
   )
 }
