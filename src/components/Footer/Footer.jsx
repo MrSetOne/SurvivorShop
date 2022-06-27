d8a091d34596e47992e7498d81439fadbae1ce4a
alextebbitt <alex.tebbitt87@gmail.com>
2022-06-27 16:36:43 +0200

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,30 @@
+import "./Footer.scss";
+import { FacebookOutlined, TwitterOutlined, YoutubeOutlined, InstagramOutlined } from "@ant-design/icons";
+
 const Footer = () => {
   return (
     <footer className="Footer">
-      <h1>Soy el footer</h1>
-      <p>&copy; 2022 ReactNews. All rights reserved.</p>
+      <div className="logos">
+        
+
+        <a href="https://www.facebook.com" target="_blank">
+          <FacebookOutlined />
+        </a>
+        <a href="https://www.twitter.com" target="_blank">
+          <TwitterOutlined />
+        </a>
+        <a href="https://www.youtube.com" target="_blank">
+          <YoutubeOutlined />
+        </a>
+        <a href="https://www.instagram.com" target="_blank">
+          <InstagramOutlined />
+        </a>
+      </div>
+      <p className="copy">
+        &copy; 2022 Survivor Shop ---(Mike & Alex)--- All rights reserved.
+      </p>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
